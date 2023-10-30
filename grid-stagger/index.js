44820ef87e04c8d1028c57cd689ab3a28606306c
Prashant <prashantrahul141@protonmail.com>
2023-10-30 18:34:41 +0530

    Update

--- grid-stagger/index.js
+++ grid-stagger/index.js
@@ -1,16 +1,16 @@
 const tileDimension = 40;
 let columns = Math.floor(window.innerWidth / tileDimension);
 let rows = Math.floor(window.innerHeight / tileDimension);
-const wrapper = document.getElementById('wrapper');
+const wrapper = document.getElementById("wrapper");
 
-const colors = ['#fff275', '#ff8c42', '#ff3c38', '#40e0d0', '#007fff'];
+const colors = ["#fff275", "#ff8c42", "#ff3c38", "#40e0d0", "#007fff"];
 let count = 0;
 
 // click event for tiles
 const handleClick = (index) => {
   count += 1;
   anime({
-    targets: '.tile',
+    targets: ".tile",
     backgroundColor: colors[count % (colors.length + 1)],
     delay: anime.stagger(15, { grid: [columns, rows], from: index }),
   });
@@ -18,19 +18,19 @@ const handleClick = (index) => {
 
 // func to create a single tile div
 const createTile = (index) => {
-  const tile = document.createElement('div');
-  tile.classList.add('tile');
+  const tile = document.createElement("div");
+  tile.classList.add("tile");
   tile.onclick = () => handleClick(index);
   return tile;
 };
 
 // to create all tiles and set col and rows css property
 const createAllTiles = (quantity) => {
-  Array.from(Array(quantity)).map((tile, index) => {
+  Array.from(Array(quantity)).map((title, index) => {
     wrapper.appendChild(createTile(index));
   });
-  wrapper.style.setProperty('--columns', columns);
-  wrapper.style.setProperty('--rows', rows);
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
 };
 
 // creating initial tiles
@@ -38,13 +38,13 @@ createAllTiles(columns * rows);
 
 // when resizing window
 const createGrid = () => {
-  wrapper.innerHTML = '';
+  wrapper.innerHTML = "";
 
   columns = Math.floor(window.innerWidth / tileDimension);
   rows = Math.floor(window.innerHeight / tileDimension);
 
-  wrapper.style.setProperty('--columns', columns);
-  wrapper.style.setProperty('--rows', rows);
+  wrapper.style.setProperty("--columns", columns);
+  wrapper.style.setProperty("--rows", rows);
   createAllTiles(columns * rows);
 };
 
